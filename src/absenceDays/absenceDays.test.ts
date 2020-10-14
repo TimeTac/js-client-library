16d90cfd2a2d68bb00d575122f6a556ef904980f
Rene Haubitzer <rene.haubitzer@timetac.com>
2020-10-14 08:08:58 +0200

    [WIP] PoC of unit test

---
+++ src/absenceDays/absenceDays.test.ts
@@ -0,0 +1,19 @@
+import AbsenceDays from './index';
+import { AbsenceDay } from './types';
+import { Absence } from '../absences/types';
+import axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
+
+var absenceDays: AbsenceDays;
+var mock = new AxiosMockAdapter(axios);
+
+describe('AbsenceDays', () => {
+  beforeAll(() => {
+    absenceDays = new AbsenceDays({});
+  });
+
+  test('read', async () => {
+    mock.onGet('https://go.timetac.com/undefined/userapi/v3/absencesDays/read').reply(200, { Success: true, NumResults: 2, Results: {} });
+    const result: Promise<AbsenceDay[]> = absenceDays.read();
+  });
+});
