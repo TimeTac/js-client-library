3cc214f0f6b7206e6bc9329a8f6253d1f8de7953
Rene Haubitzer <rene.haubitzer@timetac.com>
2020-10-15 14:21:50 +0200

    [WIP]

--- src/absenceDays/absenceDays.test.ts
+++ src/absenceDays/absenceDays.test.ts
@@ -19,14 +19,18 @@ describe('AbsenceDays', () => {
 
     mock.onGet(path).reply(200, { Success: true, NumResults: 1, Results: [{}] });
     result = absenceDays.read(new RequestParams<AbsenceDay>());
-    await result;
+    await result.then((result) => expect(result).toStrictEqual([{}]));
 
     mock.onGet(path, { params: { user_id: '1', _op__user_id: 'eq' } }).reply(200, { Success: true, NumResults: 1, Results: [{}] });
     result = absenceDays.read(new RequestParams<AbsenceDay>().eq('user_id', '1'));
-    await result;
+    await result.then((result) => expect(result).toStrictEqual([{}]));
 
-    mock.onGet(path).reply(500, { Success: false });
+    mock.onGet(path).reply(200, { Success: false });
     result = absenceDays.read(new RequestParams<AbsenceDay>());
-    await result;
+    await result.catch((result) => expect(result).toStrictEqual({ Success: false }));
+
+    mock.onGet(path).reply(500);
+    result = absenceDays.read(new RequestParams<AbsenceDay>());
+    await result.then((result) => expect(result).toBe('Request failed with status code 500'));
   });
 });
--- src/utils/responseHandlers.ts
+++ src/utils/responseHandlers.ts
@@ -3,39 +3,44 @@ import { ApiResponse } from './apiResponse';
 
 class ResponseHandler {
   toApiResponse(promise: Promise<any>) {
-    return promise.then((response) => {
-      if (!response) {
-        return Promise.reject(new Error('Request failed.'));
-      }
-      if (response.status === 200 && response.data.Success) {
-        return response.data;
-      }
-      // response.statusText is never null,
-      // rather an empty string is returned on eg. 403 => fallback to status code
-      const message = response.statusText || response.status.toString();
-      return Promise.reject(new Error(message));
-    });
+    return promise
+      .then((response: AxiosResponse) => {
+        if (response.data.Success) {
+          return response.data;
+        }
+        return Promise.reject(response);
+      })
+      .catch((error: any) => {
+        if (error.response.data!) {
+          return Promise.reject(error.response.data);
+        }
+        return Promise.reject(error.message);
+      });
   }
 
   /**
    * @return A promise that resolves to T or rejects if no results
    */
   required<T>(promise: Promise<AxiosResponse>): Promise<T> {
-    return this.toApiResponse(promise).then((response: ApiResponse<T[]>) => {
-      if (response.NumResults > 0) {
-        return response.Results[0];
-      }
-      return Promise.reject(new Error(response.ErrorMessage!));
-    });
+    return this.toApiResponse(promise)
+      .then((response: ApiResponse<T[]>) => {
+        if (response.NumResults > 0) {
+          return response.Results[0];
+        }
+        return Promise.reject(new Error(response.ErrorMessage!));
+      })
+      .catch((error) => error);
   }
 
   requiredList<T>(promise: Promise<AxiosResponse>): Promise<T[]> {
-    return this.toApiResponse(promise).then((response: ApiResponse<T[]>) => {
-      if (response.NumResults > 0) {
-        return response.Results;
-      }
-      return Promise.reject(new Error(response.ErrorMessage != null ? response.ErrorMessage : 'No results'));
-    });
+    return this.toApiResponse(promise)
+      .then((response: ApiResponse<T[]>) => {
+        if (response.NumResults >= 0) {
+          return response.Results;
+        }
+        return Promise.reject(new Error(response.ErrorMessage != null ? response.ErrorMessage : 'No results'));
+      })
+      .catch((error) => error);
   }
 
   /**
