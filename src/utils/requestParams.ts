9d3035ab7ed849f4942c54f7f876f73c677be3a4
Rene Haubitzer <rene.haubitzer@timetac.com>
2020-10-20 11:35:18 +0200

    removed String(field) by adding & string

--- src/utils/requestParams.ts
+++ src/utils/requestParams.ts
@@ -11,73 +11,73 @@ export default class RequestParams<R extends Object> {
     return this;
   }
 
-  fields<F extends keyof R>(...values: F[]): RequestParams<R> {
+  fields<F extends keyof R & string>(...values: F[]): RequestParams<R> {
     this.criteria['_fields'] = values.join(',');
     return this;
   }
 
-  eq<F extends keyof R>(field: F, value: string): RequestParams<R> {
+  eq<F extends keyof R & string>(field: F, value: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'eq';
-    this.criteria[String(field)] = value;
+    this.criteria[field] = value;
     return this;
   }
 
-  gt<F extends keyof R>(field: F, value: string): RequestParams<R> {
+  gt<F extends keyof R & string>(field: F, value: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'gt';
-    this.criteria[String(field)] = value;
+    this.criteria[field] = value;
     return this;
   }
 
-  gteq<F extends keyof R>(field: F, value: string): RequestParams<R> {
+  gteq<F extends keyof R & string>(field: F, value: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'gteq';
-    this.criteria[String(field)] = value;
+    this.criteria[field] = value;
     return this;
   }
 
-  lt<F extends keyof R>(field: F, value: string): RequestParams<R> {
+  lt<F extends keyof R & string>(field: F, value: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'lt';
-    this.criteria[String(field)] = value;
+    this.criteria[field] = value;
     return this;
   }
 
-  lteq<F extends keyof R>(field: F, value: string): RequestParams<R> {
+  lteq<F extends keyof R & string>(field: F, value: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'lteq';
-    this.criteria[String(field)] = value;
+    this.criteria[field] = value;
     return this;
   }
 
-  like<F extends keyof R>(field: F, value: string): RequestParams<R> {
+  like<F extends keyof R & string>(field: F, value: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'like';
-    this.criteria[String(field)] = value;
+    this.criteria[field] = value;
     return this;
   }
 
-  in<F extends keyof R>(field: F, ...values: string[]): RequestParams<R> {
+  in<F extends keyof R & string>(field: F, ...values: string[]): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'in';
-    this.criteria[String(field)] = values.join('|');
+    this.criteria[field] = values.join('|');
     return this;
   }
 
-  between<F extends keyof R>(field: F, min: String, max: string): RequestParams<R> {
+  between<F extends keyof R & string>(field: F, min: String, max: string): RequestParams<R> {
     this.criteria[`_op__${field}`] = 'betw';
-    this.criteria[String(field)] = [min, max].join('|');
+    this.criteria[field] = [min, max].join('|');
     return this;
   }
 
-  orderBy<F extends keyof R>(field: F, descending = false): RequestParams<R> {
-    this.criteria[`_order_by`] = String(field);
+  orderBy<F extends keyof R & string>(field: F, descending = false): RequestParams<R> {
+    this.criteria[`_order_by`] = field;
     if (descending) {
       this.criteria[`_order_desc`] = 'true';
     }
     return this;
   }
 
-  groupBy<F extends keyof R>(...fields: F[]): RequestParams<R> {
+  groupBy<F extends keyof R & string>(...fields: F[]): RequestParams<R> {
     this.criteria['_group_by'] = fields.join('|');
     return this;
   }
 
-  aggregate<F extends keyof R>(field: F, func: 'sum' | 'avg' | 'max' | 'min' | 'count' | 'group_concat'): RequestParams<R> {
+  aggregate<F extends keyof R & string>(field: F, func: 'sum' | 'avg' | 'max' | 'min' | 'count' | 'group_concat'): RequestParams<R> {
     this.criteria[`_aggregate__${field}`] = func;
     return this;
   }
