485552d503b3ae746de237e3609b6e91eb39e40c
Sergej Chak <sergej.chak@timetac.com>
2020-11-03 11:49:52 +0100

    improve fields types

--- src/userStatusOverview/types.ts
+++ src/userStatusOverview/types.ts
@@ -3,22 +3,22 @@ export type UserStatusOverview = {
   /**
    * Time tracking id of a running task
    */
-  time_tracking_id?: number;
-  time_tracking_start_time?: string;
-  time_tracking_start_time_timezone_id?: string;
-  time_tracking_task_id?: number;
-  time_tracking_is_nonworking?: boolean;
+  time_tracking_id: number;
+  time_tracking_start_time: string | null;
+  time_tracking_start_time_timezone_id: string | null;
+  time_tracking_task_id: number;
+  time_tracking_is_nonworking: boolean | null;
   /**
    * 1 if there is core time violation, default 0
    */
-  core_time_violation?: boolean;
-  core_time_start?: string;
-  updated?: string;
-  is_running?: boolean;
+  core_time_violation: boolean;
+  core_time_start: string;
+  updated: string;
+  is_running: boolean;
   /**
    * Current absence ids
    */
-  current_absence_ids?: string;
+  current_absence_ids: string | null;
   /**
    * Represents current status of users activity if he is offline (0), working(1),break(2),on leave(3), core time violation (5)
    */
