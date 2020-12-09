fcbbbc32fc1d8b1ca0a39085bc7245f1bd3b807b
Tomi Mocnik <tomaz.mocnik@timetac.com>
2020-12-09 11:22:30 +0100

    Updated index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { ApiConfig } from './baseApi';
-import { Credentials } from './authentication/types';
-import RequestParams from './utils/requestParams/requestParams';
-
 import AbsenceDays from './absenceDays';
 import Absences from './absences';
 import AbsenceTypes from './absenceTypes';
 import Authentication from './authentication';
+import { Credentials } from './authentication/types';
+import { ApiConfig } from './baseApi';
 import DeltaSync from './deltaSync';
 import Departments from './departments';
+import FavouriteTasks from './favouriteTasks';
+import GeneralSettings from './generalSettings';
 import Projects from './projects';
 import ServerCommunication from './serverCommunication';
 import Tasks from './tasks';
@@ -15,16 +15,18 @@ import TeamMembers from './teamMembers';
 import Teams from './teams';
 import TimesheetAccountings from './timesheetAccountings';
 import Timetrackings from './timetrackings';
+import TodoTasks from './todoTasks';
 import Users from './users';
 import UserStatusOverviews from './userStatusOverview';
-import GeneralSettings from './generalSettings';
+import RequestParams from './utils/requestParams/requestParams';
 
 export { AbsenceDay } from './absenceDays/types';
-export { Absence, AbsenceStatus, AbsenceApprove, AbsenceUpdate, AbsenceReject, AbsenceCreate } from './absences/types';
+export { Absence, AbsenceApprove, AbsenceCreate, AbsenceReject, AbsenceStatus, AbsenceUpdate } from './absences/types';
 export { AbsenceType } from './absenceTypes/types';
 export { Credentials } from './authentication/types';
-export { SyncResource, SyncData, SyncResourceField } from './deltaSync/types';
+export { SyncData, SyncResource, SyncResourceField } from './deltaSync/types';
 export { Department } from './departments/types';
+export { FavouriteTask, FavouriteTaskCreate } from './favouriteTasks/types';
 export { GeneralSetting } from './generalSettings/types';
 export { Project } from './projects/types';
 export { ServerCommunication } from './serverCommunication/types';
@@ -33,6 +35,7 @@ export { TeamMember } from './teamMembers/types';
 export { Team } from './teams/types';
 export { TimesheetAccounting } from './timesheetAccountings/types';
 export { TimeTracking, TimeTrackingCreate, StartTimeTrackingData, StopTimeTrackingData } from './timetrackings/types';
+export { TodoTask, TodoTaskCreate } from './todoTasks/types';
 export { User } from './users/types';
 export * from './userStatusOverview/types';
 export { RequestParams };
@@ -46,6 +49,7 @@ export default class Api {
   public authentication: Authentication;
   public deltaSync: DeltaSync;
   public departments: Departments;
+  public favouriteTasks: FavouriteTasks;
   public generalSettings: GeneralSettings;
   public projects: Projects;
   public serverCommunication: ServerCommunication;
@@ -54,6 +58,7 @@ export default class Api {
   public teams: Teams;
   public timesheetAccountings: TimesheetAccountings;
   public timeTrackings: Timetrackings;
+  public todoTasks: TodoTasks;
   public users: Users;
   public userStatusOverviews: UserStatusOverviews;
 
@@ -66,6 +71,7 @@ export default class Api {
     this.authentication = new Authentication(this.config);
     this.deltaSync = new DeltaSync(this.config);
     this.departments = new Departments(this.config);
+    this.favouriteTasks = new FavouriteTasks(this.config);
     this.generalSettings = new GeneralSettings(this.config);
     this.projects = new Projects(this.config);
     this.serverCommunication = new ServerCommunication(this.config);
@@ -74,6 +80,7 @@ export default class Api {
     this.teams = new Teams(this.config);
     this.timesheetAccountings = new TimesheetAccountings(this.config);
     this.timeTrackings = new Timetrackings(this.config);
+    this.todoTasks = new TodoTasks(this.config);
     this.users = new Users(this.config);
     this.userStatusOverviews = new UserStatusOverviews(this.config);
   }
