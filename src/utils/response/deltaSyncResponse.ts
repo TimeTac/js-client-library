40ac0e39d89d885e28cc2987155dcc0b44b7884a
Rene Haubitzer <rene.haubitzer@timetac.com>
2021-01-26 17:07:28 +0100

    added all resources to deltaSync

--- src/utils/response/deltaSyncResponse.ts
+++ src/utils/response/deltaSyncResponse.ts
@@ -1,8 +1,18 @@
 import { AxiosResponse } from 'axios';
 
+import { AbsenceDay } from '../../absenceDays/types';
 import { Absence } from '../../absences/types';
+import { AbsenceType } from '../../absenceTypes/types';
 import { DeltaSyncResults } from '../../deltaSync/types';
+import { Department } from '../../departments/types';
+import { GeneralSetting } from '../../generalSettings/types';
+import { Project } from '../../projects/types';
 import { Task } from '../../tasks/types';
+import { Team } from '../../teams/types';
+import { TimesheetAccounting } from '../../timesheetAccountings/types';
+import { TimeTracking } from '../../timetrackings/types';
+import { User } from '../../users/types';
+import { UserStatusOverview } from '../../userStatusOverview/types';
 import { RawApiResponse } from './rawApiResponse';
 import { ResourceResponse } from './resourceResponse';
 
@@ -17,9 +27,18 @@ export function createDeltaSyncResponse(rawApiResponse: RawApiResponse): DeltaSy
     success: true,
     apiResponse: rawApiResponse,
     results: {
+      absenceDays: convert<AbsenceDay>(rawApiResponse, 'absenceDays'),
       absences: convert<Absence>(rawApiResponse, 'absences'),
+      absenceTypes: convert<AbsenceType>(rawApiResponse, 'absenceTypes'),
+      department: convert<Department>(rawApiResponse, 'absenceTypes'),
+      generalSettings: convert<GeneralSetting>(rawApiResponse, 'generalSettings'),
+      projects: convert<Project>(rawApiResponse, 'projects'),
       tasks: convert<Task>(rawApiResponse, 'tasks'),
-      timeTrackings: convert(rawApiResponse, 'timeTrackings'),
+      teams: convert<Team>(rawApiResponse, 'teams'),
+      timesheetAccountings: convert<TimesheetAccounting>(rawApiResponse, 'timesheetAccountings'),
+      timeTrackings: convert<TimeTracking>(rawApiResponse, 'timeTrackings'),
+      users: convert<User>(rawApiResponse, 'users'),
+      userStatusOverview: convert<UserStatusOverview>(rawApiResponse, 'userStatusOverview'),
     },
   };
   return result;
