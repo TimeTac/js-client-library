311b5d4dace66e24dca555e9ee33e91a72f9476b
Sergej Chak <sergej.chak@timetac.com>
2021-02-04 13:50:15 +0100

    remove last_started as it is not needed for now

--- src/tasks/types.ts
+++ src/tasks/types.ts
@@ -30,7 +30,6 @@ export type Task = {
   duration?: number;
   is_startable?: boolean;
   is_paid_non_working?: boolean;
-  last_started?: string;
 };
 
 export interface TaskCreate extends Omit<Task, 'id' | 'node_path' | 'view_id'> {}
