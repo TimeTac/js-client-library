97aaf9aaa5837cc043c6d172214a66831aa38f79
Lukas Aumair <aumair.lukas@oton-media.com>
2021-02-11 12:25:48 +0100

    Bugfix

--- src/timeplannings/index.ts
+++ src/timeplannings/index.ts
@@ -6,14 +6,12 @@ import { TimePlanning } from './types';
 export class TimePlanningsEndpoint extends BaseApi {
   public readonly resourceName = 'timePlannings';
 
-  public read(requestParams?: RequestParams<TimePlanning> | Object): Promise<TimePlanning[]> {
-    const params = requestParams instanceof RequestParams ? requestParams.getParams() : requestParams;
+  public read(params?: RequestParams<TimePlanning>): Promise<TimePlanning[]> {
     const response = this._get<TimePlanning[]>(`${this.getResourceName()}/read`, { params });
     return responseHandlers.list(response);
   }
 
-  public readById(id: number, requestParams?: RequestParams<TimePlanning> | Object): Promise<TimePlanning> {
-    const params = requestParams instanceof RequestParams ? requestParams.getParams() : requestParams;
+  public readById(id: number, params?: RequestParams<TimePlanning>): Promise<TimePlanning> {
     const response = this._get<TimePlanning[]>(`${this.getResourceName()}/read/${id}`, { params });
     return responseHandlers.required(response);
   }
