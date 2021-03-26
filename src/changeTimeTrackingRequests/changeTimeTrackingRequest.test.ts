03b2f17cd191e0dab8436a48364ddb29a7569ee8
Tomaz Bevec <tomaz.bevec@timetac.com>
2021-03-26 14:08:19 +0100

    [wip] changeTimeTrackingRequestEndpoint

---
+++ src/changeTimeTrackingRequests/changeTimeTrackingRequest.test.ts
@@ -0,0 +1,168 @@
+import { describe, expect, test } from '@jest/globals';
+import axios from 'axios';
+import AxiosMockAdapter from 'axios-mock-adapter';
+import { createMock } from 'ts-auto-mock';
+
+import { ErrorReason, TimeTacApiError } from '../errors';
+import { RequestParamsBuilder } from '../utils/params/requestParams';
+import { ReadRawResponse } from '../utils/response/readRawResponse';
+import { ChangeTimeTrackingRequestEndpoint } from './index';
+import { ChangeTimeTrackingRequest } from './types';
+
+type Resource = ChangeTimeTrackingRequestEndpoint;
+const endpoint: TasksEndpoint = new TasksEndpoint({ account: 'testingAccount' });
+
+describe('tasks.read', () => {
+  const readPath = `${endpoint.getResourcePath()}/read`;
+
+  test('with status 200 and Success true and single result', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+
+    mock.onGet(readPath).reply(200, { Success: true, NumResults: 1, Results: [record] });
+    const actual: Promise<Resource[]> = endpoint.read();
+    expect(await actual).toStrictEqual([record]);
+  });
+
+  test('with status 200 and Success true and more results', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+
+    mock.onGet(readPath).reply(200, { Success: true, NumResults: 5, Results: [record, record, record, record, record] });
+    const actual: Promise<Resource[]> = endpoint.read();
+    expect(await actual).toStrictEqual([record, record, record, record, record]);
+  });
+
+  test('with status 200 and Success true and params', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const body = { params: { id: '42', _op__id: 'eq' } };
+    const requestParams = new RequestParamsBuilder<Resource>().eq('id', 42).build();
+
+    mock.onGet(readPath, body).reply(200, { Success: true, NumResults: 5, Results: [record] });
+    const actual: Promise<Resource[]> = endpoint.read(requestParams);
+    expect(await actual).toStrictEqual([record]);
+  });
+
+  test('with status 200 and Success false', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const apiResponse = { Success: false, NumResults: 1, Results: [record] };
+
+    mock.onGet(readPath).reply(200, apiResponse);
+    const actual: Promise<Resource[]> = endpoint.read();
+    expect(await actual.catch((err: typeof apiResponse) => err)).toStrictEqual(apiResponse);
+  });
+
+  test('with status 400 and Success false', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const apiResponse = { Success: false, NumResults: 1, Results: [record] };
+
+    mock.onGet(readPath).reply(400, apiResponse);
+    const actual: Promise<Resource[]> = endpoint.read();
+    expect(await actual.catch((err: { message: string }) => err.message)).toMatch('Request failed with status code 400');
+  });
+});
+
+describe('tasks.readRaw', () => {
+  const readPath = `${endpoint.getResourcePath()}/read`;
+
+  test('with status 200 and Success true and single result', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const requestParams = new RequestParamsBuilder<Resource>().build();
+
+    mock.onGet(readPath).reply(200, { Success: true, NumResults: 1, Results: [record], _ignoreTypeGuard: true });
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(requestParams);
+    const expected = {
+      data: { success: true, results: [record], deleted: [], affected: {} },
+      pages: { current: {} },
+    };
+    expect(await actual).toMatchObject(expected);
+  });
+
+  test('with status 200 and Success true and more results', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const requestParams = new RequestParamsBuilder<Resource>().build();
+
+    mock
+      .onGet(readPath)
+      .reply(200, { Success: true, NumResults: 5, Results: [record, record, record, record, record], _ignoreTypeGuard: true });
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(requestParams);
+    const expected = {
+      data: { success: true, results: [record, record, record, record, record], deleted: [], affected: {} },
+      pages: { current: {} },
+    };
+    expect(await actual).toMatchObject(expected);
+  });
+
+  test('with status 200 and Success true and params', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const body = { params: { id: '42', _op__id: 'eq' } };
+    const requestParams = new RequestParamsBuilder<Resource>().eq('id', 42).build();
+
+    mock.onGet(readPath, body).reply(200, { Success: true, NumResults: 5, Results: [record], _ignoreTypeGuard: true });
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(requestParams);
+    const expected = {
+      data: { success: true, results: [record], deleted: [], affected: {} },
+      pages: { current: { id: '42', _op__id: 'eq' } },
+    };
+    expect(await actual).toMatchObject(expected);
+  });
+
+  test('with status 200 and Success false', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const requestParams = new RequestParamsBuilder<Resource>().build();
+    const apiResponse = { Success: false, NumResults: 1, Results: [record], _ignoreTypeGuard: true };
+
+    mock.onGet(readPath).reply(200, apiResponse);
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(requestParams);
+    expect(await actual.catch((err: TimeTacApiError) => err)).toMatchObject({ reason: ErrorReason.ReponseFailed });
+  });
+
+  test('with status 400 and Success false', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const requestParams = new RequestParamsBuilder<Resource>().build();
+    const apiResponse = { Success: false, NumResults: 1, Results: [record], _ignoreTypeGuard: true };
+
+    mock.onGet(readPath).reply(400, apiResponse);
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(requestParams);
+    expect(await actual.catch((err: TimeTacApiError) => err)).toMatchObject({ reason: ErrorReason.ReponseFailed });
+  });
+
+  test('with status 200 and Success true and pages.next', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const current = new RequestParamsBuilder<Task>().limit(3).build();
+    const next = new RequestParamsBuilder<Task>().limit(3).offset(3).build();
+
+    mock.onGet(readPath).reply(200, { Success: true, NumResults: 3, Results: [record, record, record], _ignoreTypeGuard: true });
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(current);
+    const expected = {
+      data: { success: true, results: [record, record, record], deleted: [], affected: {} },
+      pages: { current, next },
+    };
+    expect(await actual).toMatchObject(expected);
+  });
+
+  test('with status 200 and Success true and pages.next and pages.prev', async () => {
+    const mock = new AxiosMockAdapter(axios);
+    const record = createMock<Resource>();
+    const prev = new RequestParamsBuilder<Task>().limit(3).offset(0).build();
+    const current = new RequestParamsBuilder<Task>().limit(3).offset(3).build();
+    const next = new RequestParamsBuilder<Task>().limit(3).offset(6).build();
+
+    mock.onGet(readPath).reply(200, { Success: true, NumResults: 3, Results: [record, record, record], _ignoreTypeGuard: true });
+    const actual: Promise<ReadRawResponse<Resource>> = endpoint.readRaw(current);
+    const expected = {
+      data: { success: true, results: [record, record, record], deleted: [], affected: {} },
+      pages: { prev, current, next },
+    };
+    expect(await actual).toMatchObject(expected);
+  });
+});
---
+++ src/changeTimeTrackingRequests/index.ts
@@ -0,0 +1,41 @@
+import BaseApi from '../baseApi';
+import { RequestParams } from '../utils/params/requestParams';
+import { createRawApiResponse } from '../utils/response/rawApiResponse';
+import { createReadRawResponse, ReadRawResponse } from '../utils/response/readRawResponse';
+import { createResourceResponse } from '../utils/response/resourceResponse';
+import * as responseHandlers from '../utils/response/responseHandlers';
+import { ChangeTimeTrackingRequest, ChangeTimeTrackingRequestCreate } from './types';
+
+export class ChangeTimeTrackingRequestEndpoint extends BaseApi {
+  public readonly resourceName = 'changetimetrackingrequests';
+
+  public read(params?: RequestParams<ChangeTimeTrackingRequest>): Promise<ChangeTimeTrackingRequest[]> {
+    const response = this._get<ChangeTimeTrackingRequest[]>(`${this.getResourceName()}/read`, { params });
+    return responseHandlers.list(response);
+  }
+
+  public async readRaw(params: RequestParams<ChangeTimeTrackingRequest>): Promise<ReadRawResponse<ChangeTimeTrackingRequest>> {
+    const response = this._get<ChangeTimeTrackingRequest[]>(`${this.getResourceName()}/read`, { params });
+    return createReadRawResponse<ChangeTimeTrackingRequest>(createResourceResponse(await createRawApiResponse(response)), params);
+  }
+
+  public readById(id: number, params?: RequestParams<ChangeTimeTrackingRequest>): Promise<ChangeTimeTrackingRequest> {
+    const response = this._get<ChangeTimeTrackingRequest[]>(`${this.getResourceName()}/read/${id}`, { params });
+    return responseHandlers.required(response);
+  }
+
+  public create(data: ChangeTimeTrackingRequestCreate): Promise<ChangeTimeTrackingRequest> {
+    const response = this._post<ChangeTimeTrackingRequest[]>(`${this.getResourceName()}/create`, data);
+    return responseHandlers.required(response);
+  }
+
+  public update(): Promise<ChangeTimeTrackingRequest> {
+    throw new Error('not Implemented');
+  }
+
+  public delete(): Promise<ChangeTimeTrackingRequest> {
+    throw new Error(
+      'not Implemented. changeTimeTracking requests can be rejected (this is through the update endpoint), but cannot be deleted.'
+    );
+  }
+}
