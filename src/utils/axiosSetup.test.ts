510a39a442749fc1363af0614db3cc0a2e50957e
Benjamin Fercher <benjamin.fercher@timetac.com>
2021-04-01 14:41:36 +0200

    Test onTokenRefreshFailed invocation

--- src/utils/axiosSetup.test.ts
+++ src/utils/axiosSetup.test.ts
@@ -1,7 +1,6 @@
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 
 import { AuthenticationEndpoint } from '../authentication';
-import { TokenResponse } from '../authentication/types';
 import { InterceptorParams, responseRejectedInterceptor } from './axiosSetup';
 
 describe('axiosSetup', () => {
@@ -23,57 +22,61 @@ describe('axiosSetup', () => {
 
     const mockAuthenticationEndpoint = new AuthenticationEndpoint(mockConfig);
 
-    mockAuthenticationEndpoint.refreshToken = (jest.fn(() =>
-      Promise.reject(responseRejectedInterceptor(new Error('Refresh token expired or something'))
-    ) as unknown) as () => Promise<AxiosResponse<TokenResponse>>;
-
     const mockInterceptorParams: InterceptorParams = {
       config: mockConfig,
       state: {
         refreshingToken: false,
       },
-      // authentication: {
-      //   config: mockedConfig,
-      //   getResourceName: jest.fn(),
-      //   getResourcePath: jest.fn(),
-      //   getTokens: jest.fn(),
-      //   login: jest.fn(),
-      //   requestTokens: jest.fn(),
-      //   refreshToken: jest.fn(),
-      //   resourceName: '',
-      //   setAccount: jest.fn(),
-      //   setClientId: jest.fn(),
-      //   setTokens: jest.fn(),
-      //   _delete: jest.fn(),
-      //   _get: jest.fn(),
-      //   _post: jest.fn(),
-      //   _put: jest.fn(),
-      //   getAccountUrl: jest.fn(),
-      //   getApiPath: jest.fn(),
-      //   getOptions: jest.fn(),
-      // },
       authentication: mockAuthenticationEndpoint,
     };
 
-    const mockError: AxiosError = {
+    const mockErrorUnauthenticated: AxiosError = {
       code: '401',
       response: {
         status: 401,
         config: { url: 'someurl' },
-        data: {},
+        data: {
+          error_description: 'The provided access token is expired.',
+          error: 'unauthorized_client',
+        },
         statusText: '',
         headers: {},
       },
       config: {},
       isAxiosError: true,
-      message: 'the error message',
+      message: 'unauthorized',
+      name: 'the name',
+      toJSON: () => ({}),
+    };
+
+    const mockErrorRefreshFailed: AxiosError = {
+      code: '497',
+      response: {
+        status: 497,
+        config: { url: 'someurl' },
+        data: {
+          error_description: 'The provided refresh token is invalid.',
+          error: 'invalid_grant',
+        },
+        statusText: '',
+        headers: {},
+      },
+      config: {},
+      isAxiosError: true,
+      message: 'refresh token request failed',
       name: 'the name',
       toJSON: () => ({}),
     };
 
     const interceptor = responseRejectedInterceptor(mockInterceptorParams);
-    await interceptor(mockError).catch((error) => {
-      //expect(error).toBeFalsy();
+
+    // The token refresh goes through the interceptor too, so we emulate that here
+    mockAuthenticationEndpoint.refreshToken = jest.fn(() => interceptor(mockErrorRefreshFailed));
+
+    expect.assertions(3);
+
+    await interceptor(mockErrorUnauthenticated).catch((error) => {
+      expect(error).toMatchObject({ message: 'refresh token request failed' });
     });
 
     // eslint-disable-next-line @typescript-eslint/unbound-method
