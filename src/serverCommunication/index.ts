5e282ed042b87cbd0b4b959a371ba409a9232df7
David Pečnik <david.pecnikk@gmail.com>
2021-04-27 12:38:33 +0200

    Added correct type for serverCommunicaton

--- src/serverCommunication/index.ts
+++ src/serverCommunication/index.ts
@@ -8,6 +8,7 @@ export class ServerCommunicationEndpoint extends BaseApi {
     this.setAccount(account);
     const response = await this._get<ServerCommunication>(`${this.resourceName}/read`, { withCredentials: false });
     if (response.data.Success && response.data.Results !== undefined) {
+      console.log('api', response.data.Results);
       return response.data.Results;
     }
 
