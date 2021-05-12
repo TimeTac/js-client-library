8997d43e5aecbf6c0299a964c34f8bfc5e9e1571
Sergej Chak <sergej.chak@timetac.com>
2021-05-12 11:20:33 +0200

    modify department entity fields and add readRow for api requests

--- src/departments/index.ts
+++ src/departments/index.ts
@@ -2,6 +2,9 @@ import BaseApi from '../baseApi';
 import { RequestParams } from '../utils/params/requestParams';
 import * as responseHandlers from '../utils/response/responseHandlers';
 import { Department } from './types';
+import { createReadRawResponse, ReadRawResponse } from '../utils/response/readRawResponse';
+import { createResourceResponse } from '../utils/response/resourceResponse';
+import { createRawApiResponse } from '../utils/response/rawApiResponse';
 
 export class DepartmentsEndpoint extends BaseApi {
   public readonly resourceName = 'departments';
@@ -9,6 +12,11 @@ export class DepartmentsEndpoint extends BaseApi {
   public read(params?: RequestParams<Department>): Promise<Department[]> {
     const response = this._get<Department[]>(`${this.getResourceName()}/read`, { params });
     return responseHandlers.list(response);
+  }
+
+  public async readRaw(params: RequestParams<Department>): Promise<ReadRawResponse<Department>> {
+    const response = this._get<Department[]>(`${this.getResourceName()}/read`, { params });
+    return createReadRawResponse<Department>(createResourceResponse(await createRawApiResponse(response)), params);
   }
 
   public readById(id: number, params?: RequestParams<Department>): Promise<Department> {
