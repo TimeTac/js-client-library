ad094722630011dd88b5971fbebde3739a7ca8db
Benjamin Fercher <benjamin.fercher@timetac.com>
2022-01-25 09:57:25 +0100

    Make resolve_user_id nullable

--- src/permissions/permissionResolveUsers/types.ts
+++ src/permissions/permissionResolveUsers/types.ts
@@ -2,7 +2,7 @@ export type PermissionResolveUser = {
   id: number;
   resolve_question_id: number;
   subject_user_id: number;
-  resolve_user_id: number;
+  resolve_user_id: number | null;
   resolve_user_wildcard: string;
   inherited_user_ids: string;
 };
