d77fe6792d49c1484215d622f776cf68e9ba9c37
Sergej Chak <sergej.chak@timetac.com>
2022-05-31 13:32:19 +0200

    make department fields required

--- src/departments/types.ts
+++ src/departments/types.ts
@@ -1,12 +1,13 @@
 export type Department = {
   id: number;
-  department_name?: string;
-  active?: boolean;
+  department_name: string;
+  active: boolean;
   supervisor_id?: number;
   supervisor_assistant_id?: number;
-  mother_id?: number;
-  view_order?: number;
-  node_path?: string;
+  mother_id: number;
+  view_order: number;
+  sort_order: number;
+  node_path: string;
   data_changed?: string;
 };
 
