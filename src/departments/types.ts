0593e57f5d62b278bb4807be8fb345f47d3ce672
Sergej Chak <sergej.chak@timetac.com>
2022-05-31 17:19:57 +0200

    prettyfix

--- src/departments/types.ts
+++ src/departments/types.ts
@@ -11,7 +11,10 @@ export type Department = {
   data_changed?: string;
 };
 
-export type DepartmentCreate = Omit<Department, 'id' | 'active' | 'view_order' | 'sort_order' | 'node_path' | 'supervisor_id' | 'supervisor_assistant_id' | 'data_changed'>;
+export type DepartmentCreate = Omit<
+  Department,
+  'id' | 'active' | 'view_order' | 'sort_order' | 'node_path' | 'supervisor_id' | 'supervisor_assistant_id' | 'data_changed'
+>;
 
 export type DepartmentUpdate = Partial<Omit<Department, 'data_changed'>> & {
   id: number;
