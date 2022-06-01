88a32c0e302cdfa52962399b501b8033c3deddc4
Siarhei Chak <sergej.tschak@gmail.com>
2022-06-01 09:44:02 +0200

    Make department fields required (#424)
    * make department fields required
    
    * fix tests run as fields need to be omitted
    
    * make lead & assistant fields be required
    
    * change department types definitions
    
    * prettyfix
    
    Co-authored-by: Sergej Chak <sergej.chak@timetac.com>

--- src/departments/types.ts
+++ src/departments/types.ts
@@ -1,20 +1,21 @@
 export type Department = {
   id: number;
-  department_name?: string;
-  active?: boolean;
-  supervisor_id?: number;
-  supervisor_assistant_id?: number;
-  mother_id?: number;
-  view_order?: number;
-  node_path?: string;
+  department_name: string;
+  active: boolean;
+  supervisor_id: number;
+  supervisor_assistant_id: number;
+  mother_id: number;
+  view_order: number;
+  sort_order: number;
+  node_path: string;
   data_changed?: string;
 };
 
-export type DepartmentCreate = Omit<Department, 'id'> & {
-  department_name: string;
-  mother_id: number;
-};
+export type DepartmentCreate = Omit<
+  Department,
+  'id' | 'active' | 'view_order' | 'sort_order' | 'node_path' | 'supervisor_id' | 'supervisor_assistant_id' | 'data_changed'
+>;
 
-export type DepartmentUpdate = Partial<Department> & {
+export type DepartmentUpdate = Partial<Omit<Department, 'data_changed'>> & {
   id: number;
 };
