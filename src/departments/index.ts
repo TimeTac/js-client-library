165d90b1aa7f55701d79f208643f14bc57321119
benj56 <benjamin.fercher@timetac.com>
2022-06-14 13:34:41 +0200

    Add departments update batch request tests (#432)
    * Add departments update batch request tests
    
    * Bump to 1.13.0

--- src/departments/index.ts
+++ src/departments/index.ts
@@ -1,7 +1,7 @@
 import BaseApi from '../baseApi';
-import { required, Required, requiredSingle } from '../utils/response/responseHandlers';
-import { Entity } from '../utils/response/apiResponse';
-import { DepartmentCreate, DepartmentUpdate } from './types';
+import { ParsedErrorMesage, Required, requiredBatch, requiredSingle } from '../utils/response/responseHandlers';
+import { LibraryReturn } from '../utils/response/apiResponse';
+import { Department, DepartmentCreate, DepartmentUpdate } from './types';
 
 const resourceName = 'departments';
 type ResourceName = typeof resourceName;
@@ -14,8 +14,23 @@ export class DepartmentsEndpoint extends BaseApi<ResourceName> {
     return requiredSingle(response);
   }
 
-  public update(data: DepartmentUpdate): Required<ResourceName, Entity<ResourceName>[]> {
-    const response = this._put<ResourceName>('update', data);
-    return required(response);
+  public async update(data: DepartmentUpdate[]): Required<typeof resourceName, (ParsedErrorMesage | Department)[]>;
+
+  public async update(data: DepartmentUpdate): Required<ResourceName>;
+
+  public async update(
+    data: DepartmentUpdate | DepartmentUpdate[]
+  ): Promise<LibraryReturn<'departments', Department> | LibraryReturn<'departments', (ParsedErrorMesage | Department)[]>>;
+
+  public async update(
+    data: DepartmentUpdate | DepartmentUpdate[]
+  ): Promise<LibraryReturn<'departments', Department> | LibraryReturn<'departments', (ParsedErrorMesage | Department)[]>> {
+    if (Array.isArray(data)) {
+      const response = this._putBatch<ResourceName>('update', data);
+      return requiredBatch(response);
+    } else {
+      const response = this._put<ResourceName>('update', data);
+      return requiredSingle(response);
+    }
   }
 }
