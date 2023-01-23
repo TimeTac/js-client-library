6c528f9de1eda82b7acc9a66405f1624706798cc
kaveh.taher <kaveh.taher@timetac.com>
2023-01-23 15:46:26 +0100

    add type of update

--- src/tasks/types.ts
+++ src/tasks/types.ts
@@ -51,6 +51,36 @@ export type Task = {
   name_path: string;
 };
 
+export type TaskUpdate = Omit<
+  Partial<Task>,
+  | 'node_path'
+  | 'view_id'
+  | 'jira_id'
+  | 'ultimate_mother_id'
+  | 'view_order'
+  | 'icon_name'
+  | 'custom_icon_name'
+  | 'initial_duration'
+  | 'object_type'
+  | 'duration'
+  | 'is_restricted'
+  | 'is_todo'
+  | 'last_started'
+  | 'target_duration_sum_up_by_task'
+  | 'color'
+  | 'has_children'
+  | 'translate_task_name'
+  | 'allow_task_project_edit'
+  | 'allow_task_project_delete'
+  | 'status'
+  | 'external_id'
+  | 'is_billable'
+  | 'is_nonworking'
+  | 'name_path'
+> & {
+  is_blocked?: boolean;
+};
+
 export type TaskCreate = Omit<
   Partial<Task>,
   'id' | 'node_path' | 'view_id' | 'ultimate_mother_id' | 'view_order' | 'icon_name' | 'object_type'
