d5ad613a3b2c8651a6469cf0d4cba28a8818bf77
Sergej Chak <sergej.chak@timetac.com>
2023-03-14 16:16:35 +0100

    Change fields order in the type definition

--- src/workScheduleDays/types.ts
+++ src/workScheduleDays/types.ts
@@ -2,7 +2,6 @@ export type WorkScheduleDay = {
   id: number;
   timesheet_template_id: number;
   weekday_id: number;
-  weekday: string;
   workday: boolean;
   half_holiday: boolean;
   hours: number;
@@ -10,6 +9,7 @@ export type WorkScheduleDay = {
   core_time_end: string;
   core_time_start_tolerance: number;
   core_time_end_tolerance: number;
+  weekday: string;
 };
 
 export type WorkScheduleDayUpdate = Omit<Partial<WorkScheduleDay>, 'timesheet_template_id' | 'weekday_id'>;
