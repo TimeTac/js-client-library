935f967e12b376cbffced77101a3dd3331cc9eaf
Sergej Chak <sergej.chak@timetac.com>
2023-03-22 16:11:16 +0100

    Allow bunch update with types defgenition

--- src/workScheduleDays/index.ts
+++ src/workScheduleDays/index.ts
@@ -9,7 +9,7 @@ type ResourceName = typeof resourceName;
 export class WorkScheduleDaysEndpoint extends BaseApi<ResourceName> {
   public readonly resourceName = resourceName;
 
-  public update(data: WorkScheduleDayUpdate): Required<ResourceName, Entity<ResourceName>[]> {
+  public update(data: WorkScheduleDayUpdate[]): Required<ResourceName, Entity<ResourceName>[]> {
     const response = this._put<ResourceName>('update', data);
     return required(response);
   }
