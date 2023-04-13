c19e1b6a0e9efd80325773c3babbf32ca48e9755
Sergej Chak <sergej.chak@timetac.com>
2023-04-13 10:24:30 +0200

    Add editable & deletable fields to work schedules

--- src/workSchedules/types.ts
+++ src/workSchedules/types.ts
@@ -2,10 +2,12 @@ export type WorkSchedule = {
   id: number;
   name: string;
   archived: number;
+  editable: boolean;
+  deletable: boolean;
   note: string;
   status: number;
 };
 
-export type WorkScheduleCreate = Omit<Partial<WorkSchedule>, 'id' | 'archived' | 'status'>;
+export type WorkScheduleCreate = Omit<Partial<WorkSchedule>, 'id' | 'archived' | 'editable' | 'deletable' | 'status'>;
 
-export type WorkScheduleUpdate = Pick<WorkSchedule, 'id'> & Omit<Partial<WorkSchedule>, 'archived' | 'status' | 'id'>;
+export type WorkScheduleUpdate = Pick<WorkSchedule, 'id'> & Omit<Partial<WorkSchedule>, 'archived' | 'editable' | 'deletable' | 'status' | 'id'>;
