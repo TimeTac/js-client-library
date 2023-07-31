e0d882dec41d9e7a398e48f1271c2bb38ba53cde
Khomenok Vladimir <34284073+khomenok@users.noreply.github.com>
2023-07-31 12:48:39 +0200

    Added law limits to workSchedules (#518)
    * added law limits to workSchedules
    
    * added law limits to workSchedules
    
    ---------
    
    Co-authored-by: Vladimir Khomenok <vladimir.khomenok@timetac.com>

--- src/workSchedules/types.ts
+++ src/workSchedules/types.ts
@@ -6,9 +6,35 @@ export type WorkSchedule = {
   deletable: boolean;
   note: string;
   status: number;
+  law_limit_daily_hours: number;
+  law_limit_weekly_hours: number;
+  law_rest_period_minimum_hours: number;
+  law_rest_period_minimum_weekly_hours: number;
 };
 
-export type WorkScheduleCreate = Omit<Partial<WorkSchedule>, 'id' | 'archived' | 'editable' | 'deletable' | 'status'>;
+export type WorkScheduleCreate = Omit<
+  Partial<WorkSchedule>,
+  | 'id'
+  | 'archived'
+  | 'editable'
+  | 'deletable'
+  | 'status'
+  | 'law_limit_daily_hours'
+  | 'law_limit_weekly_hours'
+  | 'law_rest_period_minimum_hours'
+  | 'law_rest_period_minimum_weekly_hours'
+>;
 
 export type WorkScheduleUpdate = Pick<WorkSchedule, 'id'> &
-  Omit<Partial<WorkSchedule>, 'archived' | 'editable' | 'deletable' | 'status' | 'id'>;
+  Omit<
+    Partial<WorkSchedule>,
+    | 'archived'
+    | 'editable'
+    | 'deletable'
+    | 'status'
+    | 'id'
+    | 'law_limit_daily_hours'
+    | 'law_limit_weekly_hours'
+    | 'law_rest_period_minimum_hours'
+    | 'law_rest_period_minimum_weekly_hours'
+  >;
