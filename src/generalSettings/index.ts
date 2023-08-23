5f1ac61fb0f5cbd1da0c5967a2353e587d095290
Moritz Gschwantner <moritz.gschwantner@gmail.com>
2023-08-23 13:43:15 +0200

    Add 'updateById' function to generalSettings api

--- src/generalSettings/index.ts
+++ src/generalSettings/index.ts
@@ -1,8 +1,8 @@
 import BaseApi from '../baseApi';
 import { RequestParamsBuilder } from '../utils/params/requestParams';
-import { Entity } from '../utils/response/apiResponse';
-import { required, Required } from '../utils/response/responseHandlers';
-import { GeneralSetting } from './types';
+import { Entity, LibraryReturn } from '../utils/response/apiResponse';
+import { required, Required, requiredSingle } from '../utils/response/responseHandlers';
+import { GeneralSetting, GeneralSettingUpdate } from './types';
 
 const resourceName = 'generalSettings';
 type ResourceName = typeof resourceName;
@@ -14,5 +14,10 @@ export class GeneralSettingsEndpoint extends BaseApi<ResourceName> {
     const params = new RequestParamsBuilder<GeneralSetting>().eq('setting_type', settingType).build();
     const response = this._get<ResourceName>('read', { params });
     return required(response);
+  }
+
+  public async updateById(data: GeneralSettingUpdate): Promise<LibraryReturn<ResourceName, Entity<ResourceName>>> {
+    const response = this._put<ResourceName>('update', data);
+    return requiredSingle(response);
   }
 }
