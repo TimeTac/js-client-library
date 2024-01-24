c1799af98c707872e3db87ec12fc3b9d48a97067
kaveh.taher <kaveh.taher@gmail.com>
2024-01-24 10:22:35 +0100

    fix ssoconfig types

--- src/ssoConfig/types.ts
+++ src/ssoConfig/types.ts
@@ -1,5 +1,5 @@
 export type SsoConfig = {
-  id?: number;
+  id: number;
   entity_id: string;
   login_url: string;
   logout_url: string;
@@ -14,6 +14,8 @@ export type SsoConfigUpdate = {
   login_url?: string;
   logout_url?: string;
   x509_certificate?: string;
-  open_id_login: 'O' | 'R';
-  send_email_reminders: boolean;
+  open_id_login?: 'O' | 'R';
+  send_email_reminders?: boolean;
 };
+
+export type SsoConfigCreate = Omit<SsoConfig, 'id'>;
