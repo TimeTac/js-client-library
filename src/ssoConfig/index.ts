eac358f65d212d8757760bb5908103993fb986e9
kaveh.taher <kaveh.taher@gmail.com>
2024-01-24 13:56:09 +0100

    fix ssoconfig types

--- src/ssoConfig/index.ts
+++ src/ssoConfig/index.ts
@@ -2,14 +2,14 @@ import BaseApi from '../baseApi';
 import { list, Required, requiredSingle } from '../utils/response/responseHandlers';
 import { Entity, LibraryReturn } from '../utils/response/apiResponse';
 import { RequestParams } from '../utils/params/requestParams';
-import { SsoConfig, SsoConfigUpdate } from './types';
+import { SsoConfig, SsoConfigCreate, SsoConfigUpdate } from './types';
 
 const resourceName = 'ssoConfig';
 
 export class SsoConfigEndpoint extends BaseApi<'ssoConfig'> {
   public readonly resourceName = resourceName;
 
-  public create(data: SsoConfig): Required<typeof resourceName> {
+  public create(data: SsoConfigCreate): Required<typeof resourceName> {
     const response = this._post<typeof resourceName>('create', data);
     return requiredSingle(response);
   }
