abb9643d5b731debc171478370c111a3aa881866
Aminreza Badeleh Mansoori <48173745+aminmansoori@users.noreply.github.com>
2024-03-01 13:26:27 +0100

    add a putBatch for WorkScheduleDayDefinitionUpdate (#690)
    Co-authored-by: amin mansoori <amin.mansoori@timetac.com>

--- src/workScheduleDayDefinitions/index.ts
+++ src/workScheduleDayDefinitions/index.ts
@@ -1,8 +1,8 @@
 import BaseApi from '../baseApi';
-import { Entity } from '../utils/response/apiResponse';
-import { required, Required } from '../utils/response/responseHandlers';
+import { Entity, LibraryReturn } from '../utils/response/apiResponse';
+import { ParsedErrorMesage, required, Required, requiredBatch, requiredSingle } from '../utils/response/responseHandlers';
 import { RequestParams } from '../utils/params/requestParams';
-import { WorkScheduleDayDefinitionCreate, WorkScheduleDayDefinitionUpdate } from './types';
+import { WorkScheduleDayDefinitionCreate, WorkScheduleDayDefinitionUpdate, WorkScheduleDayDefinition } from './types';
 
 const resourceName = 'workScheduleDayDefinitions';
 type ResourceName = typeof resourceName;
@@ -18,11 +18,18 @@ export class WorkScheduleDayDefinitionsEndpoint extends BaseApi<ResourceName> {
     return required(response);
   }
 
-  public update(
-    data: WorkScheduleDayDefinitionUpdate,
-    params?: RequestParams<Entity<ResourceName>>,
-  ): Required<ResourceName, Entity<ResourceName>[]> {
-    const response = this._put<ResourceName>('update', data, params);
-    return required(response);
+  public async update(
+    data: WorkScheduleDayDefinitionUpdate | WorkScheduleDayDefinitionUpdate[],
+  ): Promise<
+    | LibraryReturn<'workScheduleDayDefinitions', WorkScheduleDayDefinition>
+    | LibraryReturn<'workScheduleDayDefinitions', (ParsedErrorMesage | WorkScheduleDayDefinition)[]>
+  > {
+    if (Array.isArray(data)) {
+      const response = this._putBatch<ResourceName>('update', data);
+      return requiredBatch(response);
+    } else {
+      const response = this._put<ResourceName>('update', data);
+      return requiredSingle(response);
+    }
   }
 }
