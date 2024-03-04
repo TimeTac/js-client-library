96619e3cd1bc74c5517bf43daaca6c953780b3e4
Vladimir Khomenok <vladimir.khomenok@timetac.com>
2024-03-04 15:16:05 +0100

    skipEmail in UserUpdatePassword

--- src/users/types.ts
+++ src/users/types.ts
@@ -127,6 +127,7 @@ export interface UserUpdatePassword {
   password: string;
   old_password: string;
   revoke_access?: boolean;
+  skipEmail?: boolean;
 }
 
 export interface UserValidatePassword {
