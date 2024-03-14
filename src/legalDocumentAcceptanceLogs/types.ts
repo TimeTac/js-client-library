ce4e37972bc814b28767e017e8f68e1a93e8086c
Sergej Chak <sergej.chak@timetac.com>
2024-03-14 17:26:08 +0100

    rename field. temp changes

--- src/legalDocumentAcceptanceLogs/types.ts
+++ src/legalDocumentAcceptanceLogs/types.ts
@@ -1,6 +1,6 @@
 export type LegalDocumentAcceptanceLog = {
   id: number;
-  legal_document_type_id: number;
+  legal_document_type_id: number; // it should be legal_document_id. BE ticket has been created
   accepted: boolean;
   accepted_at: string;
   accepted_oauth_client_id: string;
