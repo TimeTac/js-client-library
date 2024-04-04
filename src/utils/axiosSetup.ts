4b38c766cdd540f85fa913c17756320d3a61f426
Tomi Močnik <tomaz.mocnik@timetac.com>
2024-04-04 13:49:07 +0200

    Handle status code 400 on authentication (#715)

--- src/utils/axiosSetup.ts
+++ src/utils/axiosSetup.ts
@@ -61,7 +61,7 @@ export const createResponseRejectedInterceptor = (interceptorParams: Interceptor
         const error = e as AxiosError;
         // Check if refresh token expired, call tokenRefreshFailed() and then re-throw error
         const status = error.response?.status;
-        if (status === 497 || status === 404 || status === 503) {
+        if (status === 497 || status === 404 || status === 503 || status === 400) {
           if (interceptorParams.config.settings.onTokenRefreshFailed != null) {
             interceptorParams.config.settings.onTokenRefreshFailed();
           }
