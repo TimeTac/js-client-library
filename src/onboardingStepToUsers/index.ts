12ab8ac3c20702329c7d0dd5de044095c7d1e4dd
Aminreza Badeleh Mansoori <48173745+aminmansoori@users.noreply.github.com>
2024-05-13 09:16:19 +0200

    the onboardingStep and touser types turned into singular (#745)
    Co-authored-by: amin <amin.mansoori@timetac.com>

--- src/onboardingStepToUsers/index.ts
+++ src/onboardingStepToUsers/index.ts
@@ -2,7 +2,7 @@ import { Entity, LibraryReturn, Resources } from '../utils/response/apiResponse'
 import { list, required, Required } from '../utils/response/responseHandlers';
 import { RequestParams } from '../utils/params/requestParams';
 import BaseApi from '../baseApi';
-import { OnboardingStepToUsersUpdate } from './types';
+import { OnboardingStepToUserUpdate } from './types';
 
 const resourceName = 'onboardingStepToUsers';
 type ResourceName = typeof resourceName;
@@ -17,7 +17,7 @@ export class OnboardingStepToUserSEndpoint extends BaseApi<ResourceName> {
     return list(response);
   }
 
-  public update(data: OnboardingStepToUsersUpdate): Required<typeof resourceName, Resources[typeof resourceName][]> {
+  public update(data: OnboardingStepToUserUpdate): Required<typeof resourceName, Resources[typeof resourceName][]> {
     const response = this._post<ResourceName>('submit', data);
     return required(response);
   }
