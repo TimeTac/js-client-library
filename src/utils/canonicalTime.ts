815ba9067f593f8c13e6cf80a3ee1353be50967e
Sergej Chak <sergej.chak@timetac.com>
2024-07-04 13:48:48 +0200

    Roll back luxon. Weird issues with request promises

--- src/utils/canonicalTime.ts
+++ src/utils/canonicalTime.ts
@@ -1,12 +1,22 @@
-import { DateTime } from 'luxon';
-
 let previousDeviation = 0;
 
 export function updateCanonicalTime(data: unknown, onServerTimeDeviationChange: ((deviation: number) => void) | undefined): void {
   if (typeof data === 'object' && data != null && 'RequestEndTime' in data) {
-    const body = data as { RequestEndTime: string };
-    const time = DateTime.fromISO(body.RequestEndTime).setZone('UTC+2');
-    const deviation = DateTime.local().toMillis() / 1000 - time.toMillis() / 1000;
+    const body = data as { RequestEndTime: string; ServerTimeZone: string | undefined };
+    const time = new Date(
+      new Intl.DateTimeFormat('en-US', {
+        timeZone: body.ServerTimeZone ?? 'Europe/Vienna',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+      }).format(new Date(body.RequestEndTime)),
+    );
+
+    const deviation = new Date().getTime() / 1000 - time.getTime() / 1000;
     if (Math.abs(deviation - previousDeviation) > 2) {
       previousDeviation = deviation;
       if (onServerTimeDeviationChange) onServerTimeDeviationChange(deviation);
