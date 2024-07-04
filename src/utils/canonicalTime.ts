4aed7527bd0f94f357dd8ef92c61a3a051d06613
Sergej Chak <sergej.chak@timetac.com>
2024-07-04 12:49:27 +0200

    Change calculation of canonical time

--- src/utils/canonicalTime.ts
+++ src/utils/canonicalTime.ts
@@ -1,10 +1,12 @@
+import { DateTime } from 'luxon';
+
 let previousDeviation = 0;
 
 export function updateCanonicalTime(data: unknown, onServerTimeDeviationChange: ((deviation: number) => void) | undefined): void {
   if (typeof data === 'object' && data != null && 'RequestEndTime' in data) {
     const body = data as { RequestEndTime: string };
-    const time = Date.parse(body.RequestEndTime);
-    const deviation = new Date().getTime() / 1000 - time / 1000;
+    const time = DateTime.fromISO(body.RequestEndTime).setZone('UTC+2');
+    const deviation = DateTime.local().toMillis() / 1000 - time.toMillis() / 1000;
     if (Math.abs(deviation - previousDeviation) > 2) {
       previousDeviation = deviation;
       if (onServerTimeDeviationChange) onServerTimeDeviationChange(deviation);
