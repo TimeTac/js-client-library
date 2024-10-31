68ca35b9ef8a1224a462e0638f0026389c3e5d4c
Sergej Chak <sergej.chak@timetac.com>
2024-10-31 15:25:38 +0100

    Add type definitions, prepare for migration

--- src/integrationsToCategories/types.ts
+++ src/integrationsToCategories/types.ts
@@ -1,7 +1,13 @@
+// Fields integrationId, categoryId, createdAt, updatedAt will be deprecated.
+// Use integration_id, category_id, created_at, updated_at instead.
 export type IntegrationToCategory = {
   id: number;
-  integrationId: number;
-  categoryId: number;
-  createdAt: string;
-  updatedAt: string;
+  integrationId?: number;
+  integration_id?: number;
+  categoryId?: number;
+  category_id?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  created_at?: string;
+  updated_at?: string;
 };
