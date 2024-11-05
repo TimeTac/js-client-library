70172478963f31716005e12186143b2be377f460
Siarhei Chak <sergej.tschak@gmail.com>
2024-11-05 13:41:49 +0100

    Add type definitions, prepare for migration to another aliases (#886)
    * Add type definitions, prepare for migration
    
    * Add type definitions, prepare for migration
    
    * Return back externalId, it will be cleaned up with the rest of fields
    
    ---------
    
    Co-authored-by: Sergej Chak <sergej.chak@timetac.com>

--- src/integrationCategories/types.ts
+++ src/integrationCategories/types.ts
@@ -1,8 +1,14 @@
+// Fields externalId, iconUrl, createdAt, updatedAt will be deprecated.
+// Use unique_id, icon_url, created_at, updated_at instead.
 export type IntegrationCategory = {
   id: number;
   title: string;
-  externalId: string;
+  externalId?: string;
+  unique_id: string;
   iconUrl?: string;
-  createdAt: string;
-  updatedAt: string;
+  icon_url?: string;
+  createdAt?: string;
+  created_at?: string;
+  updatedAt?: string;
+  updated_at?: string;
 };
