bad3bb92ce7eee29a4643a31531087c8ba47ca8f
Aleks G <aleksandr.gavrilenko@timetac.com>
2025-01-12 18:46:42 +0100

    rm deprecated fields

--- src/integrationsToCategories/types.ts
+++ src/integrationsToCategories/types.ts
@@ -1,13 +1,7 @@
-// Fields integrationId, categoryId, createdAt, updatedAt will be deprecated.
-// Use integration_id, category_id, created_at, updated_at instead.
 export type IntegrationToCategory = {
   id: number;
-  integrationId?: number;
   integration_id?: number;
-  categoryId?: number;
   category_id?: number;
-  createdAt?: string;
-  updatedAt?: string;
   created_at?: string;
   updated_at?: string;
 };
