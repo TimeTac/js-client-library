3aa916b376597d29903999f7f5f575060b1251de
kawehtaher <157904661+kawehtaher@users.noreply.github.com>
2025-02-18 18:58:20 +0100

    Typefix user event (#909)
    * Bump package version
    
    * fix types
    
    ---------
    
    Co-authored-by: kaveh.taher <kaveh.taher@gmail.com>

--- src/userEvents/index.ts
+++ src/userEvents/index.ts
@@ -1,8 +1,8 @@
 import BaseApi from '../baseApi';
-import { Entity } from '../utils/response/apiResponse';
 import { RequestParams } from '../utils/params/requestParams';
+import { Entity } from '../utils/response/apiResponse';
 import { Required, requiredSingle } from '../utils/response/responseHandlers';
-import { UserEvent } from './types';
+import { UserEventCreate } from './types';
 
 const resourceName = 'userEvents';
 type ResourceName = typeof resourceName;
@@ -10,7 +10,7 @@ type ResourceName = typeof resourceName;
 export class UserEventsEndpoint extends BaseApi<ResourceName> {
   public readonly resourceName = resourceName;
 
-  public create(data: UserEvent, params?: RequestParams<Entity<ResourceName>>): Required<ResourceName> {
+  public create(data: UserEventCreate, params?: RequestParams<Entity<ResourceName>>): Required<ResourceName> {
     const response = this._post<ResourceName>('create', data, params);
     return requiredSingle(response);
   }
