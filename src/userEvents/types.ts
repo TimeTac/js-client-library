2dcc9e47ab0aa23fa4fb0d048052954b58edd95e
kaveh.taher <kaveh.taher@gmail.com>
2025-02-18 18:52:37 +0100

    fix types

--- src/userEvents/types.ts
+++ src/userEvents/types.ts
@@ -1,6 +1,9 @@
 export type UserEvent = {
+  id: number;
   event_name: string;
   user_id: number;
   meta_text?: string;
   meta_json?: string;
 };
+
+export type UserEventCreate = Omit<UserEvent, 'id'>;
