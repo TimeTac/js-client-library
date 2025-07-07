5e1ea604caff035e7b0f10db8d916fcf42451b6c
Siarhei Chak <sergej.tschak@gmail.com>
2025-07-07 11:05:57 +0200

    Add is_request_flow_active field to the Geofence entity type (#953)
    * add is_request_flow_active to Geofence types
    
    * Bump version to v2.103.0-beta-stop-outside-of-geofences
    
    * Bump version to v2.104.1-beta-stop-outside-of-geofences
    
    * Restore version back
    
    ---------
    
    Co-authored-by: Sergej Chak <sergej.chak@timetac.com>

--- src/geoFences/types.ts
+++ src/geoFences/types.ts
@@ -5,13 +5,15 @@ type Geofence = {
   geo_long: number;
   radius: number;
   is_demo_data: boolean;
-  address?: string;
   updated: string;
+  is_request_flow_active: boolean;
+  address?: string;
 };
 
 export type GeofenceRead = Geofence;
 
-export type GeofenceCreate = Pick<Geofence, 'name' | 'geo_lat' | 'geo_long' | 'radius' | 'address'> & Partial<Pick<Geofence, 'updated'>>;
+export type GeofenceCreate = Pick<Geofence, 'name' | 'geo_lat' | 'geo_long' | 'radius' | 'address' | 'is_request_flow_active'> &
+  Partial<Pick<Geofence, 'updated'>>;
 
 export type GeofenceUpdate = Pick<Geofence, 'id'> &
-  Pick<Partial<Geofence>, 'name' | 'geo_lat' | 'geo_long' | 'radius' | 'updated' | 'address'>;
+  Pick<Partial<Geofence>, 'name' | 'geo_lat' | 'geo_long' | 'radius' | 'updated' | 'address' | 'is_request_flow_active'>;
